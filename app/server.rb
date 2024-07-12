de3728ca742f5ea99eb4abf27536a285ab9f59f3
jelenv <jelenv@users.noreply.github.com>
2024-07-12 20:57:34 +0200

    extract url path

--- app/server.rb
+++ app/server.rb
@@ -7,12 +7,20 @@ print "server started at: http://localhost:4221\n"
 
 loop do
   client_socket = server.accept
-  print "++ client connected\n"
-  print "<- | #{client_socket.gets}"
-  response = 'HTTP/1.1 200 OK'
+  request = client_socket.gets
+  print "<- | #{request}"
+  request_parts = request.split(' ')
+  method = request_parts[0]
+  path = request_parts[1]
+
+  response = ''
+  if method == 'GET' && path == '/'
+    response = 'HTTP/1.1 200 OK'
+  else
+    response = 'HTTP/1.1 404 Not Found'
+  end
+
   client_socket.puts "#{response}\r\n\r\n"
   print "-> | #{response}\n"
   client_socket.close
-  print "-- client disconnected\n"
 end
-
