332a40e63c516ce95255112132b05a23bd45c411
jelenv <jelenv@users.noreply.github.com>
2024-07-12 20:39:09 +0200

    respond with 200

--- app/server.rb
+++ app/server.rb
@@ -2,5 +2,17 @@
 
 require 'socket'
 
-server = TCPServer.new('localhost', 4221)
-client_socket, client_address = server.accept
+server = TCPServer.new 4221
+print "server started at: http://localhost:4221\n"
+
+loop do
+  client_socket = server.accept
+  print "++ client connected\n"
+  print "<- | #{client_socket.gets}"
+  response = 'HTTP/1.1 200 OK'
+  client_socket.puts "#{response}\r\n"
+  print "-> | #{response}\n"
+  client_socket.close
+  print "-- client disconnected\n"
+end
+
